decb4206942dfa1cfd835931f8a1d6fd493eb998
Benjamin Perkins <benperk@microsoft.com>
2022-04-06 19:03:59 +0200

    Update selectTemporalData.sql

--- Chapter04/Ch04Ex08/selectTemporalData.sql
+++ Chapter04/Ch04Ex08/selectTemporalData.sql
@@ -10,3 +10,10 @@ SELECT * FROM [brainwaves].[MSSQL_TemporalHistoryFor_2050106344] --DimSCENARIO
 SELECT * FROM [brainwaves].[MSSQL_TemporalHistoryFor_2098106515] --DimELECTRODE
 SELECT * FROM [brainwaves].[MSSQL_TemporalHistoryFor_2146106686] --DimFREQUENCY
 SELECT * FROM [brainwaves].[MSSQL_TemporalHistoryFor_46623209]   --DimSESSION
+--Temporal / History Tables
+SELECT * FROM [brainwaves].[DimSCENARIO]
+SELECT * FROM [brainwaves].[MSSQL_TemporalHistoryFor_2050106344] --DimSCENARIO
+SELECT * FROM [brainwaves].[DimELECTRODE]
+SELECT * FROM [brainwaves].[MSSQL_TemporalHistoryFor_2098106515] --DimELECTRODE
+SELECT * FROM [brainwaves].[DimFREQUENCY]
+SELECT * FROM [brainwaves].[MSSQL_TemporalHistoryFor_2146106686] --DimFREQUENCY
