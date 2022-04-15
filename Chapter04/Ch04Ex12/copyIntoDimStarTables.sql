22aec8f8c9fc013cae088c2dbac4cc3b98db9563
Benjamin Perkins <benperk@microsoft.com>
2022-04-15 11:09:19 +0200

    Update copyIntoDimStarTables.sql

--- Chapter04/Ch04Ex12/copyIntoDimStarTables.sql
+++ Chapter04/Ch04Ex12/copyIntoDimStarTables.sql
@@ -1,33 +1,33 @@
 COPY INTO [brainwaves].[DimMODE]
-FROM 'https://csharpguitarade.blob.core.windows.net/brainjammer/Tables/MODE.csv'
+FROM 'https://csharpguitar.blob.core.windows.net/brainjammer/Tables/MODE.csv'
 WITH (
     FILE_TYPE='CSV',
     FIRSTROW = 2
 )
 GO
 COPY INTO [brainwaves].[DimSCENARIO]
-FROM 'https://csharpguitarade.blob.core.windows.net/brainjammer/Tables/SCENARIO.csv'
+FROM 'https://csharpguitar.blob.core.windows.net/brainjammer/Tables/SCENARIO.csv'
 WITH (
     FILE_TYPE='CSV',
     FIRSTROW = 2
 )
 GO
 COPY INTO [brainwaves].[DimELECTRODE]
-FROM 'https://csharpguitarade.blob.core.windows.net/brainjammer/Tables/ELECTRODE.csv'
+FROM 'https://csharpguitar.blob.core.windows.net/brainjammer/Tables/ELECTRODE.csv'
 WITH (
     FILE_TYPE='CSV',
     FIRSTROW = 2
 )
 GO
 COPY INTO [brainwaves].[DimFREQUENCY]
-FROM 'https://csharpguitarade.blob.core.windows.net/brainjammer/Tables/FREQUENCY.csv'
+FROM 'https://csharpguitar.blob.core.windows.net/brainjammer/Tables/FREQUENCY.csv'
 WITH (
     FILE_TYPE='CSV',
     FIRSTROW = 2
 )
 GO
 COPY INTO [brainwaves].[DimSESSION]
-FROM 'https://csharpguitarade.blob.core.windows.net/brainjammer/Tables/SESSION.csv'
+FROM 'https://csharpguitar.blob.core.windows.net/brainjammer/Tables/SESSION.csv'
 WITH (
     FILE_TYPE='CSV',
     FIRSTROW = 2
