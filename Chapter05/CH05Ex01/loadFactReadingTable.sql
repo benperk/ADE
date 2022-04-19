c771a036d7efc426c81986bee80dab1bce47aedb
Benjamin Perkins <benperk@microsoft.com>
2022-04-19 12:07:00 +0200

    Create loadFactReadingTable.sql

---
+++ Chapter05/CH05Ex01/loadFactReadingTable.sql
@@ -0,0 +1,16 @@
+--DROP TABLE [brainwaves].[FactREADING] 
+
+CREATE TABLE [brainwaves].[FactREADING]
+WITH
+(
+   CLUSTERED COLUMNSTORE INDEX,
+   DISTRIBUTION = HASH ([FREQUENCY])
+)
+AS
+SELECT  se.SESSION_DATETIME, r.READING_DATETIME, 
+        s.SCENARIO, e.ELECTRODE, f.FREQUENCY, r.[VALUE]
+FROM    [brainwaves].[DimSESSION] se, [brainwaves].[TmpREADING] r, 
+        [brainwaves].[DimSCENARIO] s, [brainwaves].[DimELECTRODE] e, 
+        [brainwaves].[DimFREQUENCY] f
+WHERE   r.SESSION_ID = se.SESSION_ID AND se.SCENARIO_ID = s.SCENARIO_ID 
+        AND r.ELECTRODE_ID = e.ELECTRODE_ID AND r.FREQUENCY_ID = f.FREQUENCY_ID;
