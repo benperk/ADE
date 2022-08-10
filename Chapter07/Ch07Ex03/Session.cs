c6687cdd69bf386ab74597a96b4af0e1b8aa2b30
Benjamin Perkins <benperk@microsoft.com>
2022-08-10 15:15:49 +0200

    Create Session.cs

---
+++ Chapter07/Ch07Ex03/Session.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace brainjammer
+{    public class Brainwave
+    {
+        public Session Session { get; set; }
+    }
+    public class Session
+    {
+        public string Scenario { get; set; }
+        public List<POWReading> POWReading { get; set; }
+    }
+    public class POWReading
+    {
+        public DateTime ReadingDate { get; set; }
+        public int Counter { get; set; }
+        public List<AF3> AF3 { get; set; }
+        public List<T7> T7 { get; set; }
+        public List<Pz> Pz { get; set; }
+        public List<T8> T8 { get; set; }
+        public List<AF4> AF4 { get; set; }
+    }
+    public class AF3
+    {
+        public double THETA { get; set; }
+        public double ALPHA { get; set; }
+        public double BETA_L { get; set; }
+        public double BETA_H { get; set; }
+        public double GAMMA { get; set; }
+    }
+    public class T7
+    {
+        public double THETA { get; set; }
+        public double ALPHA { get; set; }
+        public double BETA_L { get; set; }
+        public double BETA_H { get; set; }
+        public double GAMMA { get; set; }
+    }
+    public class Pz
+    {
+        public double THETA { get; set; }
+        public double ALPHA { get; set; }
+        public double BETA_L { get; set; }
+        public double BETA_H { get; set; }
+        public double GAMMA { get; set; }
+    }
+    public class T8
+    {
+        public double THETA { get; set; }
+        public double ALPHA { get; set; }
+        public double BETA_L { get; set; }
+        public double BETA_H { get; set; }
+        public double GAMMA { get; set; }
+    }
+    public class AF4
+    {
+        public double THETA { get; set; }
+        public double ALPHA { get; set; }
+        public double BETA_L { get; set; }
+        public double BETA_H { get; set; }
+        public double GAMMA { get; set; }
+    }
+}
