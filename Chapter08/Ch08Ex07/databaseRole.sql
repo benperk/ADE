03cc761f00deed477acd6e14e23eb4fbf07dee62
Benjamin Perkins <benperk@microsoft.com>
2022-10-18 16:50:21 +0200

    Update databaseRole.sql

--- Chapter08/Ch08Ex07/databaseRole.sql
+++ Chapter08/Ch08Ex07/databaseRole.sql
@@ -1,11 +1,11 @@
 SELECT
-	r.[name] AS [Role], m.[name] AS [Member], m.Create_date AS [Created Date]	,
+  r.[name] AS [Role], m.[name] AS [Member], m.Create_date AS [Created Date],
   m.modify_Date AS [Modified Date]
 FROM
-	sys.database_role_members rm
-	JOIN sys.database_principals AS r 
+ sys.database_role_members rm
+  JOIN sys.database_principals AS r 
        ON rm.[role_principal_id] = r.[principal_id]
-	JOIN sys.database_principals AS m 
+  JOIN sys.database_principals AS m 
        ON rm.[member_principal_id] = m.[principal_id]
 WHERE
-	r.[type_desc] = 'DATABASE_ROLE'
+  r.[type_desc] = 'DATABASE_ROLE'
